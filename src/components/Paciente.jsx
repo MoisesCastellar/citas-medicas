a0557044c400488d9747e9565b37a337c38626fd
MoisesCastellar <mctecnia@gmail.com>
2023-03-30 17:22:12 -0500

    first commit - init project

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,27 @@
+const Paciente = () => {
+  return (
+        <div className="mx-5 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''} 
+            <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''} 
+            <span className="font-normal normal-case">Moises Castellar</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''} 
+            <span className="font-normal normal-case">mctecnia@gmail.com</span>
+            </p>
+            
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''} 
+            <span className="font-normal normal-case">10/03/2023</span>
+            </p>
+            
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''} 
+            <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur perferendis dolores architecto est officia ab animi perspiciatis, suscipit nisi quibusdam iure ullam iste eos velit minus. Nostrum nemo corrupti inventore!</span>
+            </p>
+        </div>
+  )
+}
+
+export default Paciente
